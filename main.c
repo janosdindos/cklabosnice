54e0a96df810679fb53887605586daadede79244
janosdindos <jdindos@gmail.com>
2024-11-06 06:38:55 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a,i;
+    char b;
+    printf("vitame vas v psacim stroji , zacni psat, vsechno krome 0, tym sa to ukonci!\n");
+    for (i=0 ; i== 10 ; i++)
+        {
+    scanf( "%c", &b );
+    printf(" %c", b);
+    }
+
+
+
+
+
+    return 0;
+}
